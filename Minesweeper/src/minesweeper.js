b19d976cb29b22cfacb7ff30d5d7ca1492c53cb4
ajnauleau <ajnauleau@gmail.com>
2018-05-28 21:58:43 +0200

    Adding Files

--- Minesweeper/src/minesweeper.js
+++ Minesweeper/src/minesweeper.js
@@ -11,13 +11,13 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     board.push(row);
   }
 
-  return board;
+ return board;
 };
 
 
- const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
 
-  const board = [];
+ const board = [];
   for (i = 0; i < numberOfRows; i++) {
     const row = [];
     for (k = 0; k < numberOfColumns; k++) {
@@ -27,17 +27,55 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   }
 
   let numberOfBombsPlaced = 0;
-  while (numberOfBombsPlaced < numberOfBombs) {
-    const randomRowIndex = Math.floor(Math.random() * numberOfRows);
-    const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
-     board[randomRowIndex][randomColumnIndex] = 'B';
-     numberOfBombsPlaced++;
+   while (numberOfBombsPlaced < numberOfBombs) {
+    let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+    let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+     if (board[randomRowIndex][randomColumnIndex] !== 'B') {
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
      //edit to add control flow, bombs on bombs err
-  }
+     }
+   }
 
   return board;
 };
 
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+
+  const neighborOffsets = [
+    [-1, -1], [-1, 0], [-1, 1],
+    [0,  -1],          [0,  1],
+    [1,  -1], [1,  0], [1,  1]
+  ];
+
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+
+  let numberOfBombs = 0;
+
+  neighborOffsets.forEach(offset => {
+
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[1];
+
+    if (neighborRowIndex >= 0 &&
+       neighborRowIndex < rowIndex &&
+       neighborColumnIndex >=0 &&
+       neighborColumnIndex < columnIndex) {
+    };
+
+    if (bombBoard[neighborRowIndex] == 'B' &&
+     bombBoard[neighborColumnIndex] == 'B' ||
+     bombBoard[neighborRowIndex][neighborColumnIndex] == 'B') {
+      numberOfBombs++;
+
+    };
+   });
+
+  return numberOfBombs;
+};
+
+const
 
 const printBoard = (board) => {
   board.map(func = (row) => {
