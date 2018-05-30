7de853f16513ffe82df29cc7b1f5e84c32630f2e
ajnauleau <ajnauleau@gmail.com>
2018-05-30 20:44:48 +0200

    Adding flipBoard to class

--- Minesweeper/src/minesweeper.js
+++ Minesweeper/src/minesweeper.js
@@ -1,4 +1,23 @@
 
+class Board {
+  constructor(numberOfRows, numberOfColumns, numberOfBombs) {
+    this._numberOfBombs = numberOfBombs;
+    this._numberOfTiles = numberOfRows * numberOfColumns;
+    this._playerBoard = Board.generatePlayerBoard(
+      numberOfRows, numberOfColumns);
+    this._bombBoard = Board.generateBombBoard(
+      numberOfRows, numberOfColumns, numberOfBombs);
+    );
+  }
+
+  get playerBoard() {
+    return this._playerBoard;
+  }
+
+  
+
+}
+
 
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 
@@ -74,18 +93,6 @@ const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
   });
 
   return numberOfBombs;
-};
-
-const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
-  if (playerBoard[rowIndex][columnIndex] !== ' ') {
-    console.log('This tile has already been flipped!');
-    return;
-  } else if (bombBoard[rowIndex][columnIndex] == 'B') {
-    playerBoard[rowIndex][columnIndex] = 'B';
-  } else {
-    playerBoard[rowIndex][columnIndex] =
-     getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
-  };
 };
 
 const printBoard = (board) => {
