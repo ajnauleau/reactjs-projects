7b8362e7bb3a9f23b22e44872bc42263b8e87913
Antoine Nauleau <ajnauleau@gmail.com>
2018-07-19 20:50:58 +0200

    Finish PLatlist.js/Playlist.css Empty Templates

--- Jammming/src/components/Playlist/Playlist.js
+++ Jammming/src/components/Playlist/Playlist.js
@@ -4,3 +4,9 @@ import './Methods/AddToPlaylist.js';
 import './Methods/ChangesToPlaylist.js';
 import './Methods/CreateThePlaylist.js';
 import './Methods/RemoveFromPlaylist.js';
+
+<div class="Playlist">
+  <input value="New Playlist"/>
+  <!-- Add a TrackList component -->
+  <a class="Playlist-save">SAVE TO SPOTIFY</a>
+</div>
