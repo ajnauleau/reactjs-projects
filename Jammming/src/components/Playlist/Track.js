5b76f9dc0de6df82770d1902f088bec72b764e5e
Antoine Nauleau <ajnauleau@gmail.com>
2018-07-19 20:56:27 +0200

    Finish Track.js/Track.css and Tracklist.js/Tracklist.css Empty Templates

--- Jammming/src/components/Playlist/Track.js
+++ Jammming/src/components/Playlist/Track.js
@@ -1,3 +1,11 @@
 
 import React from 'react';
 import 'Track.css';
+
+<div class="Track">
+  <div class="Track-information">
+    <h3><!-- track name will go here --></h3>
+    <p><!-- track artist will go here--> | <!-- track album will go here --></p>
+  </div>
+  <a class="Track-action"><!-- + or - will go here --></a>
+</div>
--- Jammming/src/components/Playlist/Tracklist.js
+++ Jammming/src/components/Playlist/Tracklist.js
@@ -1,3 +1,7 @@
 
 import React from 'react';
 import 'Tracklist.css';
+
+<div class="TrackList">
+    <!-- You will add a map method that renders a set of Track components  -->
+</div>
