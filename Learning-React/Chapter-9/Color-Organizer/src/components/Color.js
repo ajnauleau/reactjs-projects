c6b2c89dab23e770c55ddad58e10d9a2da615128
Antoine Nauleau <ajnauleau@gmail.com>
2019-09-22 17:02:39 +0200

    Get Context Done

--- Learning-React/Chapter-9/Color-Organizer/src/components/Color.js
+++ Learning-React/Chapter-9/Color-Organizer/src/components/Color.js
@@ -1,47 +1,39 @@
 import React from 'react';
 import {StarRating} from "./StarRating";
+import {rateColor, removeColor} from "../actions";
+import { PropTypes } from 'prop-types';
 const { Component } = React;
 
 export class Color extends Component {
 
-    componentWillMount() {
-        this.style = { backgroundColor: "#CCC" }
-    }
-
-    shouldComponentUpdate(nextProps) {
-        const { rating } = this.props
-        return rating !== nextProps.rating
-    }
-
-    componentWillUpdate(nextProps) {
-        const { title, rating } = this.props
-        this.style = null
-        this.refs.title.style.backgroundColor = "red"
-        this.refs.title.style.color = "white"
-        alert(`${title}: rating ${rating} -> ${nextProps.rating}`)
-    }
-
-    componentDidUpdate(prevProps) {
-        const { title, rating } = this.props
-        const status = (rating > prevProps.rating) ? 'better' : 'worse'
-        console.log(`${title} is getting ${status}`)
-        this.refs.title.style.backgroundColor = ""
-        this.refs.title.style.color = "black"
-    }
-
     render() {
-        const { title, color, rating, onRate } = this.props;
+        const { store } = this.context
+        const { id, title, color, rating, onRate } = this.props;
         return (
             <section className="color" style={this.style}>
                 <h1 ref="title">{title}</h1>
+                <button onClick={() =>
+                    store.dispatch(
+                        removeColor(id)
+                    )
+                }>X</button>
                 <div className="color"
                      style={{ backgroundColor: color }}>
                     <p></p>
                 </div>
                 <div>
-                    <StarRating starsSelected={rating} onRate={onRate} />
+                    <StarRating starsSelected={rating}
+                                onRate={rating =>
+                                    store.dispatch(
+                                        rateColor(id, rating)
+                                        )
+                                    } />
                 </div>
             </section>
         )
     }
+}
+
+Color.contextTypes = {
+    store: PropTypes.object
 }
